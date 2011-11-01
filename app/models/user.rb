c2044498d9a740d31e116c78611ef1fe5e749f2e
Greg Ayer <chaulky@gmail.com>
2011-10-31 22:50:33 -0400

    Added validation for presence of user name and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  
+  validates :name, :presence => true
+  validates :email, :presence => true
 end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,3 @@
-require 'spec_helper'
-
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
 # == Schema Information
 #
 # Table name: users
@@ -14,4 +8,28 @@ end
 #  created_at :datetime
 #  updated_at :datetime
 #
+require 'spec_helper'
+
+describe User do
+  
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+  
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+end
+
+
 
